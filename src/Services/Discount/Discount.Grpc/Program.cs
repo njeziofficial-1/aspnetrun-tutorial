1a05d568737d0b929f1bb18bc9392255e671f134
John Njezi <john.njezi@toyingroupsystems.com>
2024-01-07 23:20:18 +0100

    Implemented discount gRPC

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,3 +1,5 @@
+using Discount.Grpc.Extensions;
+using Discount.Grpc.Repositories;
 using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,11 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
-
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
+app.MigrateDatabase<Program>();
 app.Run();
---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services;
+
+public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+{
+    readonly ILogger<DiscountService> _logger;
+    readonly IDiscountRepository _repository;
+    readonly IMapper _mapper;
+
+    public DiscountService(ILogger<DiscountService> logger, IDiscountRepository repository, IMapper mapper)
+    {
+        _logger = logger;
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await _repository.GetDiscount(request.ProductName);
+        return coupon == null
+            ? throw new RpcException(new Status(StatusCode.NotFound, $"Could not find discount with product name = {request.ProductName}"))
+            : _mapper.Map<CouponModel>(coupon);
+    }
+
+    public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = _mapper.Map<Coupon>(request.Coupon);
+        await _repository.CreateDiscount(coupon);
+        string message = $"Discount is successfully created. ProductName:{coupon.ProductName}";
+        _logger.LogInformation(message);
+        return _mapper.Map<CouponModel>(coupon);
+    }
+
+    public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = _mapper.Map<Coupon>(request.Coupon);
+        await _repository.UpdateDiscount(coupon);
+        _logger.LogInformation($"Discount is successfully updated. ProductName:{coupon.ProductName}");
+        return _mapper.Map<CouponModel>(coupon);
+    }
+
+    public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        var deleted = await _repository.DeleteDiscount(request.ProductName);
+        return new DeleteDiscountResponse
+        {
+            Success = deleted,
+        };
+    }
+}
